964faeb373d285669b22c199a003aeb7b319eb7c
mac126 <mac126@mac126demacbook-air.local>
2018-04-02 11:24:58 +0800

    添加swipe手势 extension

--- ARKitPhysics/ARKitPhysics/ViewController.swift
+++ ARKitPhysics/ARKitPhysics/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController {
 
     @IBOutlet var sceneView: ARSCNView!
     
@@ -66,11 +66,18 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
     
     func addTapGestureToSceneView() {
-        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.addRocketshipToSceneView(recognizer:)))
+        sceneView.addGestureRecognizer(tapGesture)
     }
     
     func addSwipeGestureToSceneView() {
+        let swipeUpGesture = UISwipeGestureRecognizer(target: self, action: #selector(self.applyForceToRocketship(recognizer:)))
+        swipeUpGesture.direction = .up
+        sceneView.addGestureRecognizer(swipeUpGesture)
         
+        let swipeDownGesture = UISwipeGestureRecognizer(target: self, action: #selector(self.launchRocketship(recognizer:)))
+        swipeDownGesture.direction = .down
+        sceneView.addGestureRecognizer(swipeDownGesture)
     }
     
     func configureLighting() {
@@ -80,17 +87,25 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.autoenablesDefaultLighting = true
     }
     
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+    /// 添加火箭
+    @objc func addRocketshipToSceneView(recognizer: UIGestureRecognizer) {
+        print("addRocketshipToSceneView")
     }
-*/
     
+    /// 添加作用力
+    @objc func applyForceToRocketship(recognizer: UIGestureRecognizer) {
+        print("applyForceToRocketship")
+    }
+    
+    /// 发射火箭
+    @objc func launchRocketship(recognizer: UIGestureRecognizer) {
+        print("launchRocketship")
+    }
+    
+    
+}
+
+extension ViewController: ARSessionObserver {
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
         
@@ -104,5 +119,34 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
+    }
+}
+
+// MARK: - ARSCNViewDelegate
+
+extension ViewController: ARSCNViewDelegate {
+    /*
+     // Override to create and configure nodes for anchors added to the view's session.
+     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+     let node = SCNNode()
+     
+     return node
+     }
+     */
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+    }
+}
+
+extension float4x4 {
+    var translation: float3 {
+        let translation = self.columns.3
+        return float3(x: translation.x, y: translation.y, z: translation.z)
+    }
+}
+
+extension UIColor {
+    open class var transparentWhite: UIColor {
+        return UIColor.white.withAlphaComponent(0.2)
     }
 }
